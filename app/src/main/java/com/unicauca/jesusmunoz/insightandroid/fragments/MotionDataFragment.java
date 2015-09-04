d496801e8ebe75ccf12375b6252f58289190f1bf
JesusEduardo2028 <jesusduardo2028@gmail.com>
2015-09-04 18:29:00 -0500

    new motion data fragment

---
+++ app/src/main/java/com/unicauca/jesusmunoz/insightandroid/fragments/MotionDataFragment.java
@@ -0,0 +1,45 @@
+package com.unicauca.jesusmunoz.insightandroid.fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.unicauca.jesusmunoz.insightandroid.R;
+
+/**
+ * Created by jesuseduardomunoz on 9/4/15.
+ */
+public class MotionDataFragment extends Fragment {
+    public static String TAG = "MOTION_DATA";
+
+    public MotionDataFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_motion_data, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
