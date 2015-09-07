19ddbc0ce7d689d9b66985655d6f45111031e70a
JesusEduardo2028 <jesusduardo2028@gmail.com>
2015-09-07 14:57:03 -0500

    add engagementBoredom dataset

--- app/src/main/java/com/unicauca/jesusmunoz/insightandroid/fragments/PerformanceMetrics.java
+++ app/src/main/java/com/unicauca/jesusmunoz/insightandroid/fragments/PerformanceMetrics.java
@@ -14,6 +14,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.unicauca.jesusmunoz.insightandroid.R;
 import com.unicauca.jesusmunoz.main.Graph;
 import com.unicauca.jesusmunoz.services.EmotivService;
@@ -30,12 +39,9 @@ public class PerformanceMetrics extends Fragment {
     TextView tv_excitementLongTermScore;
     TextView tv_stressScore;
     TextView tv_interestScore;
-    RelativeLayout rl_boredom_container;
-    
+    private LineChart engagementBoredomChart;
+    LineDataSet data_engagementBoredom;
 
-
-
-    Graph graphEngagementBoredom;
 
     public PerformanceMetrics() {
         // Required empty public constructor
@@ -47,6 +53,7 @@ public class PerformanceMetrics extends Fragment {
         receiver = new InsightReceiver();
         filter = new IntentFilter("EMO_STATE_FILTER");
         filter.addCategory(Intent.CATEGORY_DEFAULT);
+
     }
 
     @Override
@@ -74,15 +81,41 @@ public class PerformanceMetrics extends Fragment {
         tv_instantaneousExcitementScore = (TextView) root.findViewById(R.id.tv_exins);
         tv_stressScore = (TextView) root.findViewById(R.id.tv_stress_score);
         tv_interestScore = (TextView) root.findViewById(R.id.tv_interest_score);
-        rl_boredom_container = (RelativeLayout) root.findViewById(R.id.rl_boredom_container);
 
-        Point pt = new Point();
-        getActivity().getWindowManager().getDefaultDisplay().getSize(pt);
-        int graphWidth = pt.x - 10;
-        graphEngagementBoredom = new Graph(getActivity(), graphWidth, Color.rgb(146, 211, 238));
-        graphEngagementBoredom.setScale(1f);
-        graphEngagementBoredom.setHeight(200f);
-        rl_boredom_container.addView(graphEngagementBoredom);
+        engagementBoredomChart = (LineChart) root.findViewById(R.id.chart_engagementBoredom);
+
+        engagementBoredomChart.setDescription("");
+        engagementBoredomChart.setNoDataTextDescription("No data for the moment");
+        engagementBoredomChart.setHighlightEnabled(true);
+        engagementBoredomChart.setTouchEnabled(true);
+        engagementBoredomChart.setDragEnabled(true);
+        engagementBoredomChart.setDragEnabled(true);
+        engagementBoredomChart.setDrawGridBackground(false);
+        engagementBoredomChart.setPinchZoom(true);
+        engagementBoredomChart.setBackgroundColor(Color.LTGRAY);
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+
+        engagementBoredomChart.setData(data);
+
+        Legend l = engagementBoredomChart.getLegend();
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.WHITE);
+
+
+        XAxis x1   = engagementBoredomChart.getXAxis();
+        x1.setTextColor(Color.WHITE);
+        x1.setDrawGridLines(false);
+        x1.setAvoidFirstLastClipping(true);
+
+        YAxis y1 = engagementBoredomChart.getAxisLeft();
+        y1.setTextColor(Color.WHITE);
+        y1.setAxisMaxValue(1f);
+        y1.setDrawGridLines(true);
+
+        YAxis y2 = engagementBoredomChart.getAxisRight();
+        y2.setEnabled(false);
 
 
         return root;
@@ -112,8 +145,16 @@ public class PerformanceMetrics extends Fragment {
                 float relaxationScore = intent.getFloatExtra(EmotivService.RELAXATION_SCORE, 0);
                 float stressScore = intent.getFloatExtra(EmotivService.STRESS_SCORE, 0);
                 float interestScore = intent.getFloatExtra(EmotivService.INTEREST_SCORE, 0);
+                addEntry();
+                    //graphEngagementBoredom.addData(engagementBoredomScore)
+                // ;
+                //LineData data = engagementBoredomChart.getData();
+                //LineDataSet set = data.getDataSetByIndex(0);
 
-                graphEngagementBoredom.addData(engagementBoredomScore);
+                //data.addEntry(new Entry(engagementBoredomScore,set.getEntryCount()),0);
+                //engagementBoredomChart.notifyDataSetChanged();
+                //engagementBoredomChart.setVisibleXRangeMaximum(6);
+                //engagementBoredomChart.moveViewToX(data.getXValCount() - 7);
                 tv_relaxationScore.setText(relaxationScore + "");
                 tv_engagementBoredomScore.setText(engagementBoredomScore + "");
                 tv_excitementLongTermScore.setText(excitementLongTermScore + "");
@@ -121,6 +162,40 @@ public class PerformanceMetrics extends Fragment {
                 tv_stressScore.setText(stressScore + "");
                 tv_interestScore.setText(interestScore + "");
             }
+
+        public void addEntry(){
+            if (engagementBoredomChart != null) {
+                LineData data = engagementBoredomChart.getData();
+                if (data != null) {
+                    LineDataSet set = data.getDataSetByIndex(0);
+                    if (set == null) {
+                        set = createSet();
+                        data.addDataSet(set);
+                    }
+                    data.addXValue("");
+                    data.addEntry(new Entry((float) Math.random(), set.getEntryCount()), 0);
+                    engagementBoredomChart.notifyDataSetChanged();
+                    engagementBoredomChart.setVisibleXRange(0, 6);
+                    engagementBoredomChart.moveViewToX(data.getXValCount() - 7);
+
+                }
+
+            }
+        }
+        public LineDataSet createSet(){
+            LineDataSet set = new LineDataSet(null, "engagementBoredom");
+            set.setDrawCubic(true);
+            set.setAxisDependency(YAxis.AxisDependency.LEFT);
+            set.setColor(ColorTemplate.getHoloBlue());
+            set.setLineWidth(2f);
+            set.setCircleSize(4f);
+            set.setFillColor(ColorTemplate.getHoloBlue());
+            set.setHighLightColor(Color.rgb(244, 117, 117));
+            set.setValueTextColor(Color.WHITE);
+            set.setValueTextSize(10f);
+
+            return set;
+        }
 
         }
 }
